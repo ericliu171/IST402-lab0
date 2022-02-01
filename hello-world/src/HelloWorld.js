1e2a091e6491877b9f90323de61c2e0c5688d1e9
ericliu171 <67081386+ericliu171@users.noreply.github.com>
2022-02-01 11:30:43 -0500

    Update HelloWorld.js

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -1,7 +1,7 @@
-import { html, css, LitElement } from 'lit';
+import { html, css, LitElement } from 'lit'; //I think this imports the needed files
 
 export class HelloWorld extends LitElement {
-  static get styles() {
+  static get styles() { // I think this block of code determines what the button will look like, where on the screen it will be placed, what color it is, etc. 
     return css`
       :host {
         display: block;
@@ -11,24 +11,24 @@ export class HelloWorld extends LitElement {
     `;
   }
 
-  static get properties() {
+  static get properties() { //I think this block of code determines the types of the objects on the screen 
     return {
       title: { type: String },
       counter: { type: Number },
     };
   }
 
-  constructor() {
+  constructor() { //not really sure what this block of code does, althogh I'm assuming that the "this.counter" determiens what number the code starts at
     super();
     this.title = 'Hey there';
     this.counter = 5;
   }
 
-  __increment() {
+  __increment() { //determines what happens when the button is pressed
     this.counter += 1;
   }
 
-  render() {
+  render() { //I'm guessing this is the main function, the funciton that renders everything and combiens all of the methods and objects above
     return html`
       <h2>${this.title} Nr. ${this.counter}!</h2>
       <button @click=${this.__increment}>increment</button>
